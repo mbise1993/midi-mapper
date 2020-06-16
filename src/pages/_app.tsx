19fafe8ae2b86810e64d593d63510bc9b13336c0
Matt Bise <mbise1993@gmail.com>
2020-06-16 01:35:42 -0500

    Rename

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,7 +15,7 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
-        <title>ReMapper</title>
+        <title>MIDI Mapper</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
